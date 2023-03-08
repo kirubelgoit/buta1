2ff6d0028cdf319babb448540319e275d5d93cad
kirubelgoit <126629809+kirubelgoit@users.noreply.github.com>
2023-03-08 14:34:19 -0800

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
   <div className="gpt3__header section__padding coolBackground" id="home">
     <div className="gpt3__header-content">
       <h1 className="gradient__text">Buy from the source</h1>
-      <p>We at Buta Delivery try our best to present the finest products with reasonable prices gaving you a great products and saving you money! </p>
+      <p>We at Buta Delivery try our best to present the finest products with reasonable prices giving you a great product and saving you money! </p>
 
       <div className="gpt3__header-content__people">
        <p>Tomorrow has arrived</p>
